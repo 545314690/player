86fd73cf5eeaeaa26d4b8777274036484abc6752
李森淼 <lism@lisenmiaodeMBP2.lan>
2024-07-14 20:45:32 +0800

    mod:songs路径

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/player/'
-      : '/'
+  publicPath: process.env.NODE_ENV === '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //     ? '/player/'
+  //     : '/'
 })
 
